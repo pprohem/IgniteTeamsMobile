0fa7d853c69c3a7f6de57cfeea9ce770440082ba
pprohem <pprg00@gmail.com>
2023-02-01 23:27:31 -0300

    First Commit Ignite Teams

---
+++ IgniteTeams/App.tsx
@@ -0,0 +1,29 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { Groups } from './src/screens/Groups';
+import theme from './src/theme';
+
+export default function App() {
+    
+  
+  const [fontsLoaded] =
+  useFonts({
+      Roboto_400Regular,
+      Roboto_700Bold
+    });
+
+
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {fontsLoaded ? <Groups/> : <ActivityIndicator/>}
+      </ThemeProvider>
+      
+    </>
+   
+
+  )
+}
+
---
+++ IgniteTeams/src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+
+export  function Groups() {
+  return (
+    <S.Container>
+      
+    </S.Container>
+  );
+}
+
