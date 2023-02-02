eaa258939efbca41a49269f5029c4def95a182fa
pprohem <pprg00@gmail.com>
2023-02-02 09:52:45 -0300

    Listagem de Grupos

--- IgniteTeams/src/screens/Groups/index.tsx
+++ IgniteTeams/src/screens/Groups/index.tsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { GroupCard } from "../../components/GroupCard";
 import { Header } from "../../components/Header";
 import { Highlight } from "../../components/Highlight";
 import { Container } from "./styles";
 
 
 export  function Groups() {
+    const [groups, setGroups] = useState<string[]>([
+      'Turma de Karatê da minha Avó',
+      'Feijoada do Fabricio',
+      'Feira de Adoção de Cobra Coral',
+      'Equipe de Som do Wesley Safadão',
+      'Gerencia do Carrefour',
+      'Anões do Pânico']);
+
   return (
     <Container>
       <Header />
+      
       <Highlight title="Turmas" subtitle="Jogue com a sua turma!"/>
+      
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+        <GroupCard 
+        title={item}
+        />
+      )}
+      
+      />
+
+
+
+     
     </Container>
   );
 }
